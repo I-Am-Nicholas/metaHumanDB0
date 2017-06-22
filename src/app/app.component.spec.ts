586894a77d4edce3b92a0d2fe8a2214019dee2d1
Nicholas Leacock <nicholas@swissmail.com>
2017-06-22 15:23:40 +0100

    Refactor test so TestBd.createComponent() is not repeated in each block, by using beforeEach.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let comp: AppComponent
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    // }).compileComponents();
+    })
+
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+
   }));
 
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have given text as title`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have the given text as title`, async(() => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('META-HUMAN DATABASE');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('META-HUMAN DATABASE');
