9d6b5ee86f077287d6e5bd80e6a23c7a54075309
Nicholas Leacock <nicholas@swissmail.com>
2017-07-11 14:33:32 +0100

    Add/Remove appropriate logo images. Change dashboard selection in dashboard.component.ts Alter hover actions on dashboard buttons via component stylesheet. Adjust metas logo properties in meta-list.ts for dashboard display changes.
    Add/Remove appropriate logo images.
    Change dashboard selection in dashboard.component.ts
    Alter hover actions on dashboard buttons via component stylesheet.
    Adjust metas logo properties in meta-list.ts for dashboard display changes.

--- src/app/meta-list-local.ts
+++ src/app/meta-list-local.ts
@@ -6,9 +6,9 @@ export const METAS: Meta[] = [
   {id: 3, name: 'Hulk', logo: 'assets/gamma.jpg' },
   {id: 4, name: 'Spider-Man', logo: 'assets/spider.jpg' },
   {id: 5, name: 'Thor', logo: 'assets/mjolnir.jpg' },
-  {id: 6, name: 'Black Panther', logo: 'assets/panther.jpg' },
-  {id: 7, name: 'Scarlet Witch', logo: 'assets/scarlet.jpg' },
-  {id: 8, name: 'Hawkeye', logo: 'assets/arrow.jpg' },
-  {id: 9, name: 'Black Widow', logo: 'assets/spider2.jpg' },
+  {id: 6, name: 'Black Panther', logo: 'assets/pantera.jpg' },
+  {id: 7, name: 'Scarlet Witch', logo: 'assets/scarlet.png' },
+  {id: 8, name: 'Hawkeye', logo: 'assets/hawk.png' },
+  {id: 9, name: 'Black Widow', logo: 'assets/spider-bw.png' },
   {id: 10, name: 'Vision', logo: 'assets/gem.jpg' }
 ];
