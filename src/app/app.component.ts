20c2f2fd5f7dbda51b7304abc9f83b3431f435fb
Nicholas Leacock <nicholas@swissmail.com>
2017-07-14 17:42:55 +0100

    Add video file for new background. Re-style all text due to new vid background.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MetaService } from './meta-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', './sharedBG.css'],
   providers: [MetaService]
 })
 
