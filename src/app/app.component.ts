7594a98827701530abc1cf1817f5a7e372670a76
Nicholas Leacock <nicholas@swissmail.com>
2017-07-15 11:02:16 +0100

    Restored lost video styling, template element to dashboard BG. Restyled headings and buttons due to video addition.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MetaService } from './meta-service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', "sharedBG.css"],
   providers: [MetaService]
 })
 
